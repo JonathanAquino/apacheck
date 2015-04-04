6cbf2d8e749b9f5aa754a7209805ffc717661861
Jonathan Aquino <jonathan.aquino@gmail.com>
2015-04-03 20:33:17 -0700

    Add files for the js version of apacheck, with unit tests.

---
+++ apacheck.js
@@ -0,0 +1,136 @@
+/**
+ * Miscellaneous checks for conformity with APA citation and format style.
+ *
+ * @see
+ */
+var apacheck = {
+    rules: {
+        sample: {
+            description: 'A description of the rule.',
+            /**
+             * Returns a list of excerpts for each violation of the rule.
+             *
+             * @param string body The portion of the document before the "References" heading.
+             * @param string references The portion of the document after the "References" heading.
+             *
+             * @return Array|null An array of short excerpts around each violation.
+             */
+            check: function (body, references) {
+                return [];
+            }
+        },
+        noCommaBeforeEtAl: {
+            description: 'Do not put a comma before "et al."',
+            check: function (body, references) {
+                var regexp = XRegExp(
+                    '('
+                  +     '.{0,10}'          // 10 characters of context
+                  +     ', et al\\.'       // , et al.
+                  +     '.{0,10}'          // 10 characters of context
+                  + ')'
+                , 'g');
+                return body.match(regexp) || [];
+            }
+        },
+        periodAfterDateInReferences: {
+            description: 'References: Put period after date.',
+            check: function (body, references) {
+                var regexp = XRegExp(
+                    '('
+                  +     '.{0,10}'                   // 10 characters of context
+                  +     '\\(\\d\\d\\d\\d[^)]*\\)'   // The date
+                  +     '[^.]'                      // No period
+                  +     '.{0,10}'                   // 10 characters of context
+                  + ')'
+                , 'g');
+                return references.match(regexp) || [];
+            }
+        },
+        ampersandBeforeLastAuthorInReferences: {
+            description: 'References: Put & before last author.',
+            check: function (body, references) {
+                var regexp = XRegExp(
+                    '('
+                  +     '^'                     // Start of the line
+                  +     '[^(,]+'                // Non-comma characters
+                  +     ','                     // Comma
+                  +     '[^(,]+'                // Non-comma characters
+                  +     ','                     // Comma
+                  +     '('
+                  +         '(?!\\. \\. \\.)'
+                  +         '(?!\\.\\.\\.)'     // Does not already have & or . . .
+                  +         '[^(&]'
+                  +     ')+'
+                  +     '\\('                   // (
+                  + ')'
+                , 'g');
+                return references.match(regexp) || [];
+            }
+        },
+        lowercasedTitleInReferences: {
+            description: 'References: Consider lowercasing the words in this title if it is for an article.',
+            check: function (body, references) {
+                var regexp = XRegExp(
+                    '('
+                  +     '\\)\\.\\s*' // ). followed by whitespace
+                  +     '[A-Z]'      // First letter is uppercase
+                  +     '[^.:]*'     // Any characters other than . or :
+                  +     '[A-Z]'      // Uppercase letter
+                  +     '[^.]*'      // Any characters other than .
+                  +     '\\.'        // .
+                  + ')'
+                , 'g');
+                return references.match(regexp) || [];
+            }
+        },
+        periodFollowedBySpace: {
+            description: 'Every period should be followed by a space.',
+            check: function (body, references) {
+                var regexp = XRegExp(
+                    '('
+                  +     '.{0,10}'     // 10 characters of context
+                  +     '\\.[^ ,\n]'  // Period followed by something other than a space, comma, or newline
+                  + ')'
+                , 'g');
+                return (body + ' ' + references).match(regexp) || [];
+            }
+        }
+    },
+    /**
+     * Returns an object whose keys are rule descriptions and values are arrays
+     * of excerpts around each violation.
+     *
+     * @param string s The document to check.
+     *
+     * @return Object Error excerpts keyed by rule descriptions.
+     */
+    check: function (s) {
+        var parts = this.split(s);
+        var results = {};
+        $.each(this.rules, function (name, rule) {
+            var excerpts = rule.check(parts.body, parts.references);
+            if (excerpts.length > 0) {
+                results[rule.description] = excerpts;
+            }
+        })
+        return results;
+    },
+    /**
+     * Splits the document into two parts: body (before the "References" heading)
+     * and references (after the "References" heading).
+     *
+     * @param string s The document to check.
+     *
+     * @return Object Hash with two keys: body and references.
+     */
+    split: function (s) {
+        var match = XRegExp.exec(s, XRegExp('^\\s*References\\s*$', 'm'));
+        if (!match) {
+            return {body: s, references: ''};
+        }
+        return {
+            body: s.substring(0, match.index).trim(),
+            references: s.substring(match.index + match[0].length).trim()
+        };
+    }
+}
---
+++ test.js
@@ -0,0 +1,99 @@
+describe('noCommaBeforeEtAl', function() {
+
+    var rule = apacheck.rules.noCommaBeforeEtAl;
+
+    it('passes if there is no comma before et al.', function() {
+        var result = rule.check('Foo et al.', '');
+        expect(result).toEqual([]);
+    });
+
+    it('fails if there is a comma before et al.', function() {
+        var result = rule.check('Foo, et al.', '');
+        expect(result).toEqual(['Foo, et al.']);
+    });
+
+});
+
+describe('periodAfterDateInReferences', function() {
+
+    var rule = apacheck.rules.periodAfterDateInReferences;
+
+    it('passes if there is a period after the date.', function() {
+        var result = rule.check('', 'Foo. (2006, January). Blah.');
+        expect(result).toEqual([]);
+    });
+
+    it('fails if there is no period after the date.', function() {
+        var result = rule.check('', 'Foo. (2006, January) Blah.');
+        expect(result).toEqual(['Foo. (2006, January) Blah.']);
+    });
+
+});
+
+describe('ampersandBeforeLastAuthorInReferences', function() {
+
+    var rule = apacheck.rules.ampersandBeforeLastAuthorInReferences;
+
+    it('passes if there is an ampersand before the last author.', function() {
+        var result = rule.check('', 'Foo, F., Bar, B., & Qux, Q. (2006)');
+        expect(result).toEqual([]);
+    });
+
+    it('fails if there is no ampersand before the last author.', function() {
+        var result = rule.check('', 'Foo, F., Bar, B., Qux, Q. (2006)');
+        expect(result).toEqual(['Foo, F., Bar, B., Qux, Q. (']);
+    });
+
+});
+
+describe('lowercasedTitleInReferences', function() {
+
+    var rule = apacheck.rules.lowercasedTitleInReferences;
+
+    it('passes if the title is lowercased.', function() {
+        var result = rule.check('', 'Foo, F. (2006). The box: A retrospective.');
+        expect(result).toEqual([]);
+    });
+
+    it('fails if the title is not lowercased.', function() {
+        var result = rule.check('', 'Foo, F. (2006). The Box: A retrospective.');
+        expect(result).toEqual(['). The Box: A retrospective.']);
+    });
+
+});
+
+describe('periodFollowedBySpace', function() {
+
+    var rule = apacheck.rules.periodFollowedBySpace;
+
+    it('passes if periods are followed by spaces.', function() {
+        var result = rule.check('Hello. World. Foo.', '');
+        expect(result).toEqual([]);
+    });
+
+    it('fails if if periods are not followed by spaces.', function() {
+        var result = rule.check('Hello.World.Foo.', '');
+        expect(result).toEqual(['Hello.W', 'orld.F']);
+    });
+
+});
+
+describe('split', function() {
+
+    it('passes if split returns body and references.', function() {
+        var result = apacheck.split('This is (References) the body.\nReferences \nThese are the references.');
+        expect(result).toEqual({body: 'This is (References) the body.', references: 'These are the references.'});
+    });
+
+    it('passes if split returns empty string for references if no references.', function() {
+        var result = apacheck.split('This is (References) the body.');
+        expect(result).toEqual({body: 'This is (References) the body.', references: ''});
+    });
+
+    it('passes if split returns empty string for body if no body.', function() {
+        var result = apacheck.split('References \nThese are the references.');
+        expect(result).toEqual({body: '', references: 'These are the references.'});
+    });
+
+});
+
